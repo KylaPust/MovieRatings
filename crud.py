6e06a8833fa11a173a340b4b2305663a99f0ad06
KylaPust <mikyla.pust@capella.edu>
2022-07-16 20:44:52 -0500

    Lab 4 Sat Evening

--- crud.py
+++ crud.py
@@ -9,6 +9,23 @@ def create_user(email2, password2):
 
     return user
 
+def get_user_by_id(user_id):
+
+    user = User.query.get(user_id)
+
+    return user
+
+def get_user_by_email(email):
+
+    user = User.query.filter_by(email=email).first()
+
+    return user
+
+def get_all_users():
+
+    all_users = User.query.all()
+
+    return all_users
 
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
@@ -36,10 +53,16 @@ def get_movie_by_id(movie_id):
 
 def create_rating(user, movie, score):
 
-    rating = Rating(score=score,
-                    user=user,
-                    movie=movie)
+    rating = Rating(user=user,
+                    movie=movie,
+                    score=score,)
     
+    return rating
+
+def get_rating_by_user_movie(user, movie):
+
+    rating = Rating.user.has(user_id=user, movie_id=movie)
+
     return rating
 
 if __name__ == '__main__':
